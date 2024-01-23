6e6cdad3a4532291dfa01a08cb494d362082cec8
jtaren <arenjatau@gmail.com>
2024-01-23 21:43:40 +0100

    app.py

--- app.py
+++ app.py
@@ -116,6 +116,7 @@ else:
     # they need to put down 20%
 # Print the down payment
 
+
 """
 price = 1000000
 has_good_credit = False
@@ -128,4 +129,130 @@ print(f"Down payment: ${down_payment}")
 """
 
 # Logical Operators
+"""
+# If applicant has high income AND good credit eligible for loan
+has_high_income = True
+has_good_credit = True
 
+if has_high_income and has_good_credit:
+    print("Eligible for loan")
+# If applicant has high income OR good credit eligible for loan
+has_high_income = True
+has_good_credit = True
+
+if has_high_income or has_good_credit:
+    print("Eligible for loan")
+
+# AND: both
+# OR: at least one
+# NOT:
+
+# If applicant good credit doesn't have a criminal record
+
+has_good_credit = True
+has_criminal_record = True
+
+if has_good_credit and not has_criminal_record:
+    print("Eligible for loan")
+"""
+
+# Comparison Operators
+"""
+# If temperature is greater than 30
+    # it's a hot day
+# Othherwise if it's less than 10
+    # it's a cold day
+# Otherwise
+    # It's neither hot nor cold
+
+temperature = 35
+if temperature > 30:
+    print("It's a hot day")
+else:
+    print("It's not a hot day")
+
+# If name is less than 3 characters long 
+    # name must be at least 3 characters
+# otherwise if it's more than 50 characters long
+    # name can be a maximum of 50 characters
+# otherwise
+    # name looks good!
+
+name = "Jkkkk"
+
+if len(name) < 3:
+    print("Name must be at least 3 characters")
+elif len(name) > 50:
+    print("Name must be at maximum of 50 characters")
+else:
+    print("Name looks good!")
+"""
+
+# Weight Converter
+"""
+# weight: 72
+# (L)bs or (K)g: k
+# You are 160.0 pounds
+
+weight = int(input("weight: "))
+unit = input("(L)bs or (K)g: ")
+if unit.upper() == "L":
+    converted = weight * 0.45
+    print(f"You are {converted} kilos")
+else:
+    converted = weight / 0.45
+    print(f"You are {converted} pounds")
+"""
+
+# while loops
+"""i = 1
+while i <= 5:
+    print(i)
+    i = i + 1
+print("Done")
+
+i = 1
+while i <= 5:
+    print('*' * i) 
+    i = i + 1
+
+# Guessing Game
+secret_number = 9
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guess = int(input('Guess: '))
+    guess_count += 1
+    if guess == secret_number:
+        print("You won!")
+        break
+else:
+    print("Sorry, you failed!")
+"""
+# Car Game
+command = ""
+started = False
+while True:
+    command = input("> ").lower()
+    if command == "start":
+        if started:
+            print("Car is already started")
+        else:
+            started = True
+            print("Car started...")
+    elif command == "stop":
+        if not started:
+            print("Car is already stopped!")
+        else:
+            started = False
+            print("Car stopped")
+    elif command == "help":
+        print("""
+start - to start the car
+stop - to stop the car
+quit - to quit
+        """)
+    elif command == "quit":
+        break
+    else:
+        print("Sorry, I don't understand that!")
