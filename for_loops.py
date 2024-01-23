3d8fd3262bae6182b85428cce830e8042a3bba96
jtaren <arenjatau@gmail.com>
2024-01-23 22:03:49 +0100

    for_loops.p

---
+++ for_loops.py
@@ -0,0 +1,25 @@
+# For Loops
+"""
+for item in "Python":
+    print(item)
+
+for item in ['Mosh', 'John', 'Sarah']:
+    print(item)
+
+for item in [1, 2, 3, 4]:
+    print(item)
+
+for item in range(10):
+    print(item)
+
+prices = [10, 20, 30]
+total = 0
+for price in prices:
+    total += price
+print(f"Total:{total}")
+"""
+
+# Nested Loop
+for x in range(4):
+    for  y in range(3):
+        print(f"({x}, {y})")
