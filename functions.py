65ba37e3198b244283f2b3192f53037dd112750b
jtaren <arenjatau@gmail.com>
2024-01-26 21:14:10 +0100

    functions.py

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-# Functions
+# Functions and parameters
 """def greet_user(name):
     print(f'Hi {name}!')
     print('Welcome aboard')
@@ -7,7 +7,7 @@ greet_user("John")
 greet_user("Mary")
 print("Finish")
 """
- 
+# Parameters
 """
 def greet_user(first_name, last_name):
     print(f'Hi {first_name} {last_name}!')
@@ -16,7 +16,7 @@ print("Start")
 greet_user("John", "Smith")
 print("Finish")
 """
-
+# keyword Argument
 """
 def greet_user(first_name, last_name):
     print(f'Hi {first_name} {last_name}!')
@@ -31,11 +31,11 @@ def greet_user(first_name, last_name):
     print(f'Hi {first_name} {last_name}!')
     print('Welcome aboard')
 print("Start")
-greet_user("John", "Smith")
+greet_user("John", "Smith") # Positional argument first, then keyword arguments
 calc_cost(total=50, shipping=5, discount=0.1)
 print("Finish")
 """
-
+# Return Statement
 """
 def square(number):
     return number * number
@@ -43,7 +43,7 @@ def square(number):
 result = square(3)
 print(result)
 """
-
+# Creting a Reusable Function 
 """
 def emoji_converter(message):
     
